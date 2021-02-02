0ae4e9fb84796a6b0f22c86dc24f9d2bc74ef34b
yezii <1126yezy@gmail.com>
2021-02-02 21:39:47 +0900

    Prisma setup

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "hii",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 
