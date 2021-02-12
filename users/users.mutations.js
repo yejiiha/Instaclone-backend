fc825b11353bc80b1249ac696fd717116f85aad0
yezii <1126yezy@gmail.com>
2021-02-12 23:29:02 +0900

    seeProfile and throw error on createAccount

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -7,31 +7,39 @@ export default {
       _,
       { firstName, lastName, username, email, password }
     ) => {
-      // check if username or email are already on DB
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [
-            {
-              username,
-            },
-            {
-              email,
-            },
-          ],
-        },
-      });
-      // hash password
-      const uglyPassword = await bcrypt.hash(password, 10);
-      // save and return the user
-      return client.user.create({
-        data: {
-          username,
-          email,
-          firstName,
-          lastName,
-          password: uglyPassword,
-        },
-      });
+      try {
+        // check if username or email are already on DB
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+
+        if (existingUser) {
+          throw new Error("This username or email is already taken.");
+        }
+        // hash password
+        const uglyPassword = await bcrypt.hash(password, 10);
+        // save and return the user
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
