58c1d5d416aff3244bef204257b1ae6ecd831c73
yezii <1126yezy@gmail.com>
2021-02-13 00:14:55 +0900

    Login and Refactor

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
 
 export default {
@@ -40,6 +41,32 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      // Find user with args.username
+      const user = await client.user.findFirst({ where: { username } });
+
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      // Check password with args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      // Issue a token and send it to the user
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
