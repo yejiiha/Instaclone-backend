dd9a820a995ca1d177afe9a4c3f61be4907e07b8
yezii <1126yezy@gmail.com>
2021-02-25 17:29:38 +0900

    seeFeed

---
+++ photos/seeFeed/seeFeed.resolvers.js
@@ -0,0 +1,30 @@
+import client from "../../client";
+import { protectedResolvers } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeFeed: protectedResolvers((_, __, { loggedInUser }) =>
+      client.photo.findMany({
+        where: {
+          OR: [
+            {
+              user: {
+                followers: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+            },
+            {
+              userId: loggedInUser.id,
+            },
+          ],
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      })
+    ),
+  },
+};
