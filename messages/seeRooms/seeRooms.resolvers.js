0bae5e88da2afdfaa70093eaa242dc843135107c
yezii <1126yezy@gmail.com>
2021-02-27 22:03:17 +0900

    seeRooms

---
+++ messages/seeRooms/seeRooms.resolvers.js
@@ -0,0 +1,18 @@
+import client from "../../client";
+import { protectedResolvers } from "../../users/users.utils";
+
+export default {
+  Query: {
+    seeRooms: protectedResolvers(async (_, __, { loggedInUser }) =>
+      client.room.findMany({
+        where: {
+          users: {
+            some: {
+              id: loggedInUser.id,
+            },
+          },
+        },
+      })
+    ),
+  },
+};
