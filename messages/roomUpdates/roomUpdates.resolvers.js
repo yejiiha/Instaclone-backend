35c8c26a676ae2396262112c205603da62f8eba7
yezii <1126yezy@gmail.com>
2021-03-02 15:40:17 +0900

    Filtering Subscription

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,9 +1,32 @@
+import { withFilter } from "apollo-server";
+import client from "../../client";
+import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: {
+            id: args.id,
+          },
+          select: {
+            id: true,
+          },
+        });
+
+        if (!room) {
+          throw new Error("This room is not exist.");
+        }
+
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates }, { id }) => {
+            return roomUpdates.roomId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
